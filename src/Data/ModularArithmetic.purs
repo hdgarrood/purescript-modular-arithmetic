77af45b5a4e6cc59186192f2ecb033525be103fd
Harry Garrood <harry@garrood.me>
2017-01-29 00:36:36 +0000

    Fix EuclideanRing Z instance

--- src/Data/ModularArithmetic.purs
+++ src/Data/ModularArithmetic.purs
@@ -7,13 +7,18 @@ module Data.ModularArithmetic
   ) where
 
 import Prelude
+import Data.Array as Array
 import Type.Proxy
 import Data.Typelevel.Num (class Mul, class Nat, class Pos, class Succ, type (:*), D0, D1, D11, D2, D3, D5, D7, toInt)
 import Data.Typelevel.Undefined (undefined)
+import Test.QuickCheck (class Arbitrary)
+import Test.QuickCheck.Gen (elements)
 
 -- | Integers modulo some number m.
 newtype Z m = Z Int
 
+derive newtype instance eqZ :: Eq (Z m)
+derive newtype instance ordZ :: Ord (Z m)
 derive newtype instance showZ :: Show (Z m)
 
 mkZ :: forall m. (Pos m) => Int -> Z m
@@ -61,9 +66,11 @@ instance ringZ :: (Pos m) => Ring (Z m) where
 instance commutativeRingZ :: (Pos m) => CommutativeRing (Z m)
 
 instance euclideanRingZ :: (Prime m) => EuclideanRing (Z m) where
-  degree = runZ
+  degree _ = 1
   div x y = x * inverse y
-  mod x y = x - (div x y)
+  mod _ _ = Z 0
+
+instance fieldZ :: (Prime m) => Field (Z m)
 
 -- | Compute a multiplicative inverse of some number in Z_m. Note that an
 -- | inverse is only guaranteed to exist if m is prime (which is required by a
@@ -71,14 +78,21 @@ instance euclideanRingZ :: (Prime m) => EuclideanRing (Z m) where
 -- |
 -- | Adapted from https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Modular_integers
 inverse :: forall m. (Prime m) => Z m -> Z m
-inverse (Z a) = Z (go 0 n 1 a)
+inverse (Z a) = mkZ (go 0 n 1 a)
   where
     n = toInt (undefined :: m)
 
     go t _ _ 0 =
-      if t < 0 then t + n else t
+      t
     go t r newt newr =
       let
         quot = r / newr
       in
-        go newt (t - quot * newt) newr (r - quot * newr)
+        go newt newr (t - quot * newt) (r - quot * newr)
+
+instance arbitraryZ :: (Pos m) => Arbitrary (Z m) where
+  arbitrary =
+    let
+      m = toInt (undefined :: m)
+    in
+      map mkZ (elements 0 (Array.range 1 (m - 1)))
--- test/Main.purs
+++ test/Main.purs
@@ -3,13 +3,30 @@ module Test.Main where
 import Prelude
 import Data.ModularArithmetic
 import Control.Monad.Eff.Console (logShow)
-import Data.Typelevel.Num (D7)
+import Data.Typelevel.Num (D11)
+import Test.QuickCheck (class Arbitrary, Result, arbitrary, quickCheck, (<?>))
+import Test.QuickCheck.Gen (elements)
+import Test.QuickCheck.Laws.Data (checkCommutativeRing, checkEuclideanRing, checkField, checkRing, checkSemiring)
+import Type.Proxy (Proxy(..))
 
-mkZ7 :: Int -> Z D7
-mkZ7 = mkZ
+type Z11 = Z D11
 
 main = do
-  logShow $ (mkZ7 4 + mkZ7 2)
-  logShow $ (mkZ7 6 * mkZ7 5)
-  logShow $ (mkZ7 2 - mkZ7 4)
-  logShow $ (mkZ7 3 / mkZ7 4)
+  let p = Proxy :: Proxy Z11
+
+  quickCheck (inverses :: Z11 -> _)
+
+  checkSemiring p
+  checkRing p
+  checkCommutativeRing p
+  checkEuclideanRing p
+  checkField p
+
+inverses :: forall m. (Prime m) => Z m -> Result
+inverses x = go <?> "x: " <> show x
+  where
+    go
+      | x /= zero =
+          x * inverse x == one
+      | otherwise =
+          true
